c15538c42701dfb6683e6abd2cc452b5ed01e1d3
nyan-tama <shake.flake.love@gmail.com>
2023-02-02 17:51:06 +0900

    finish 2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-const message: string = "Hello World!";
 
-console.log(message);
+const startTime = performance.now(); // 開始時間
+
+
+for (let i: number = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log("鮭フレーク")
+        continue
+    } else if (i % 5 === 0) {
+        console.log("フレーク")
+        continue
+    } else if (i % 3 === 0) {
+        console.log("鮭")
+        continue
+    } else {
+        console.log(i)
+    }
+}
+
+const endTime = performance.now(); // 終了時間
+
+console.log(endTime - startTime); // 何ミリ秒かかったかを表示する
